7bb6af882b1121f13fcc67ef7c26ddf6ec17a82b
SHAHID786s <46031114+SHAHID786s@users.noreply.github.com>
2018-12-21 16:18:42 +0000

    Add files via upload

---
+++ Assignment_working with GUI commands.ino
@@ -0,0 +1,124 @@
+/*
+   Demo line-following code for the Pololu Zumo Robot
+
+   This code will follow a black line on a white background, using a
+   PID-based algorithm.  It works decently on courses with smooth, 6"
+   radius curves and has been tested with Zumos using 30:1 HP and
+   75:1 HP motors.  Modifications might be required for it to work
+   well on different courses or with different motors.
+
+   https://www.pololu.com/catalog/product/2506
+   https://www.pololu.com
+   https://forum.pololu.com
+*/
+
+#include <Wire.h>
+#include <ZumoShield.h>
+#define QTR_THRESHOLD 850 // microseconds
+#define NUM_SENSORS 6
+ZumoBuzzer buzzer;
+ZumoReflectanceSensorArray reflectanceSensors(QTR_NO_EMITTER_PIN);
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON);
+
+int lastError = 0;
+//Initialise sensor array to hold 6 sensors;
+unsigned int sensor_values[NUM_SENSORS];
+bool stop1 = false;
+
+//get manual commands
+char incomingByte = ' ';
+
+void setup()
+{
+
+
+  Serial.begin(9600); // Start serial communication at 9600 bps
+  calibrateZumo();
+};
+
+void loop()
+{
+  // special case when wall is detected get out of the Autmation loop then use manual mode
+  reflectanceSensors.read(sensor_values); // reads raw values from sensor
+  zumoManual();
+
+}
+
+void calibrateZumo()
+{
+
+  // Initialize the reflectance sensors module
+  reflectanceSensors.init();
+  button.waitForButton();
+
+  // Turn on LED to indicate we are in calibration mode
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  // Wait 1 second and then begin automatic sensor calibration
+  // by rotating in place to sweep the sensors over the line
+  delay(1000);
+  int i;
+  for (i = 0; i < 80; i++)
+  {
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
+      motors.setSpeeds(-200, 200);
+    else
+      motors.setSpeeds(200, -200);
+    reflectanceSensors.calibrate();
+
+    // Since our counter runs to 80, the total delay will be
+    // 80*20 = 1600 ms.
+    delay(20);
+  }
+  motors.setSpeeds(0, 0);
+
+  // Turn off LED to indicate we are through with calibration
+  digitalWrite(13, LOW);
+  button.waitForButton();
+  buzzer.play(">g32>>c32");
+}
+
+void zumoManual()
+{
+
+  motors.setSpeeds(0, 0);
+  delay(200);
+  digitalWrite(13, HIGH);
+
+  while (Serial.available()) {
+    // do MANUAL CONTROL ONCE THE ZUMO_BOT HITS THE WALL
+    incomingByte = Serial.read(); //read and store value from gui
+    if (incomingByte == 'L')
+    {
+      motors.setSpeeds(-200, 600);
+      delay(45);
+    }
+    else if (incomingByte == 'R')
+    {
+      motors.setSpeeds(600, -200);
+      delay(45);
+    }
+    else if (incomingByte == 'F')
+    {
+      motors.setSpeeds(200, 200);
+      delay(45);
+    }
+    else if (incomingByte == 'B')
+    {
+      motors.setSpeeds(-200, -200);
+      delay(30);
+    }
+    else if (incomingByte == 'C')
+    {
+      calibrateZumo();
+    }
+
+  }
+
+}
+
+
+
+
